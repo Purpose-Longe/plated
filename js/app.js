33c80e15634181599699a29d219d382e732c4a7c
Purpose Longe <purposelonge@gmail.com>
2026-02-08 16:12:46 +0100

    DOM setup

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const userSection = document.getElementById("user");
+const recipeSection = document.getElementById("recipe");
+const refreshButton = document.getElementById("refresh");
+
+console.log(userSection);
+console.log(recipeSection);
+console.log(refreshButton);
+
+refreshButton.addEventListener("click", () => {
+    console.log("Re-Plate");
+});
