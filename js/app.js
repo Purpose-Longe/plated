58b3c2b0cf038e359cca93b5c33f55ef7e7e98f1
Purpose Longe <purposelonge@gmail.com>
2026-02-08 18:01:04 +0100

    worked with spoonacular api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const SPOONACULAR_API_KEY = "783225f8d6e6482a93953b86b97b16b9";
+
 async function getRandomUser() {
     const response = await fetch("https://randomuser.me/api/");
     const data = await response.json();
@@ -12,3 +14,19 @@ async function getRandomUser() {
 
 }
 getRandomUser();
+
+async function getRandomRecipe(){
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?number=1&apiKey=${SPOONACULAR_API_KEY}`);
+    const data = await response.json();
+    const recipe = data.recipes[0];
+
+    const recipeInfo = {
+        title: recipe.title,
+        image: recipe.image,
+        readyInMinutes: recipe.readyInMinutes,
+        servings: recipe.servings
+    };
+    console.log(recipeInfo);
+
+}
+getRandomRecipe();
